510fa00ed2e1094dae60f2339ae09f50e4bc30d9
karlaGO <karla@cetos.io>
2023-08-17 15:19:46 -0600

    V1.4 Back to Version 1.2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
 /**Material modules */
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatIconModule} from '@angular/material/icon';
 import {MatListModule} from '@angular/material/list';
-import {MatCardModule} from '@angular/material/card';
-import {MatFormFieldModule} from '@angular/material/form-field';
 
 /**Components */
 import { SidenavComponent } from './sidenav/sidenav.component';
-import { HomeComponent } from './sidenav/home/home.component';
+import { HomeComponent } from './home/home.component';
 import { MatTableModule } from '@angular/material/table';
 import {MatButtonModule} from '@angular/material/button';
 import { HttpClientModule } from '@angular/common/http'; /* component for us an api  */
@@ -21,23 +18,34 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CostumersComponent } from './costumers/costumers.component';
 import { ItemsComponent } from './items/items.component';
-import { LoginComponent } from './login/login.component';
 import { SharedModule } from './shared/shared.module';
 
 
 
 @NgModule({
   declarations: [
-    LoginComponent
+    AppComponent,
+    SidenavComponent,
+    HomeComponent,
+    CostumersComponent,
+    ItemsComponent
   ],
   imports: [
-    SharedModule,
-    RouterModule.forRoot([
-      { path: '', component: LoginComponent },
-      { path: 'sidenav', component: SidenavComponent}]),
+    BrowserModule,
+    AppRoutingModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatListModule,
+    MatButtonModule,
+    MatTableModule,
+    HttpClientModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
 
